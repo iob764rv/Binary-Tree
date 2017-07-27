9b64492a5cdc76163ba74df3a4d894dde349d8e9
iob764rv <iic287m@gmail.com>
2017-07-27 19:07:54 -0400

    Update Tree.java

--- Tree.java
+++ Tree.java
@@ -8,10 +8,20 @@ public class Tree {
     NodeInTree rootNode;
     public static void main(String[] args) {
         Tree tree1 = new Tree();
-        tree1.addToTree(7);
+        tree1.addToTree(5);
         tree1.addToTree(2);
         tree1.addToTree(1);
         tree1.addToTree(4);
+        tree1.traverseTree(tree1.rootNode);
+    }
+    public void traverseTree(NodeInTree node2){//traverse the tree starting with the least value
+        //starts with root, prints values if !null
+        if(node2!=null){
+            traverseTree(node2.leftNode);
+            System.out.println(node2.value);
+            traverseTree(node2.rightNode);
+        }
+    
     }
     public void addToTree(int value){//adding nodes to the tree
         NodeInTree node= new NodeInTree(value);
@@ -28,20 +38,17 @@ public class Tree {
                     if (currentNode==null){
                         parentNode.leftNode=node;//adds to left if less
                         return;//exit loop
-                    }
-                    else{
-                        currentNode=currentNode.rightNode;
+                    }   
+                }
+                else{
+                    currentNode=currentNode.rightNode;
                         if (currentNode==null){
                             parentNode.rightNode=node;
                             return;//exit loop
                         }
                     }
-                }
             }
         }
-    }
-      public void traverseTree(NodeInTree tree){
-    
     }
     
 }
