eb3fd046ac83bb52d6733530367283435689484a
iob764rv <iic287m@gmail.com>
2017-08-04 19:27:33 -0400

    Create heap.rb

---
+++ heap.rb
@@ -0,0 +1,25 @@
+def swap(index1, index2, heap)
+  return heap
+end
+
+def pnode(index)
+  return (index-1)/2
+end
+
+def rightLeaf(index)
+    return 2*index+2
+end
+
+def leftLeaf(index)
+    return 2*index+1
+end
+
+def addToHeap(value, heap)
+
+  if heap.to_a.empty?
+		heap[0]=value #root node
+  else
+
+	end
+
+end
