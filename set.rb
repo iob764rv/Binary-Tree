87e02fd800b841dcd8d690026df92be304fbf5a4
iob764rv <iic287m@gmail.com>
2018-04-26 21:34:31 -0400

    Create set.rb

---
+++ set.rb
@@ -0,0 +1,11 @@
+main.rb
+def capital()
+  listCities=Hash["City1"=>"City1Key","City2"=>"City2Key"]
+  keys=listCities.keys
+  randomKey=keys[rand(0..listCities.size-1)]
+  
+  puts "Key of #{}"
+
+end
+
+capital()
